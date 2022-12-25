ca59f32ca9bfdb832799d1a6aa4d0a33d58f988e
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2022-12-25 09:17:56 +0200

    added BootStrap and object to iterate to show articles

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.set('view engine', 'ejs');
 app.use('/articles',articleRouter)
 
 app.get("/", (req,res)=>{
-    res.render('index')
+    const articles = [{
+        title: 'New Article',
+        date: Date.now(),
+        description: 'Test description!'
+    }]
+    res.render('index', {articles: articles})
 });
 
 app.listen(5000, console.log("Server is listening..."));
